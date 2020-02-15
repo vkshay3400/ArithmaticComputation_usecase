8f1700ac0917103bbc0ea08b19be1f985931c0a3
vkshay3400 <akshayp2208@gmail.com>
2020-02-15 16:02:29 +0530

    Added: Arithmetic Inputs

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -x
 
 echo "*************************ArithmaticComputation*************************"
+
+# THREE INPUTS FOR PERFORMING OPERATIONS
+read -p "Enter first number input: " firstnumber
+read -p "Enter second number input: " secondnumber
+read -p "Enter third number input: " thirdnumber
+
