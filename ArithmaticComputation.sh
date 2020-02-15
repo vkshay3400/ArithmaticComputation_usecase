37704816bead55acb8f3519ceba4ceb5bf1cc434
vkshay3400 <akshayp2208@gmail.com>
2020-02-15 15:39:22 +0530

    Added: Program for three input

---
+++ ArithmaticComputation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+# THREE INPUTS FOR PERFORMING OPERATIONS
+read -p "Enter first number input: " firstnumber
+read -p "Enter second number input: " secondnumber
+read -p "Enter  third number input: " thirdnumber
+
