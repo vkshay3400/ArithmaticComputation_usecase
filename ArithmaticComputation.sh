fc2582a4b4a025bd313c689bb69946832e89ad0f
vkshay3400 <akshayp2208@gmail.com>
2020-02-15 17:16:10 +0530

    Added: Read values from dictionary

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -11,7 +11,7 @@ read -p "Enter third number input: " thirdnumber
 Operation1=$(( $firstnumber + $secondnumber * $thirdnumber ))
 Operation2=$(( $firstnumber * $secondnumber + $thirdnumber ))
 Operation3=`echo "scale=2; $thirdnumber + $firstnumber / $secondnumber" | bc`
-Operation4=`echo "scale=2; $firstnumber % $secondnumber + $thirdnumber " | bc`
+Operation4=`echo "scale=2; $firstnumber % $secondnumber + $thirdnumber" | bc`
 
 # DISPLAY OPERATION
 echo "Operations Output: " $Operation1 $Operation2 $Operation3 $Operation4 
@@ -23,5 +23,5 @@ arithmetic[2]="$Operation2"
 arithmetic[3]="$Operation3"
 arithmetic[4]="$Operation4"
 
-
-
+# DISPLAY DICTIONARY
+echo "Arithmetic Operations in Dictionary: " ${arithmetic[@]}
