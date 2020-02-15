d8b980e33166b5bd75df4f791a01a358e2766013
vkshay3400 <akshayp2208@gmail.com>
2020-02-15 16:18:18 +0530

    Added: Arithmetic Operation for a+b*c

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -7,3 +7,6 @@ read -p "Enter first number input: " firstnumber
 read -p "Enter second number input: " secondnumber
 read -p "Enter third number input: " thirdnumber
 
+# ARITHMETIC OPERATION
+Operation1=$(( $firstnumber + $secondnumber * $thirdnumber ))
+
