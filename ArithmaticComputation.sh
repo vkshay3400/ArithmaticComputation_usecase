32a7a2c0306d0f779021b2b112b540fe5b609e86
vkshay3400 <akshayp2208@gmail.com>
2020-02-15 17:08:48 +0530

    Added: Arithmetic Store in dictionary

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -12,3 +12,16 @@ Operation1=$(( $firstnumber + $secondnumber * $thirdnumber ))
 Operation2=$(( $firstnumber * $secondnumber + $thirdnumber ))
 Operation3=`echo "scale=2; $thirdnumber + $firstnumber / $secondnumber" | bc`
 Operation4=`echo "scale=2; $firstnumber % $secondnumber + $thirdnumber " | bc`
+
+# DISPLAY OPERATION
+echo "Operations Output: " $Operation1 $Operation2 $Operation3 $Operation4 
+
+# STORE IN DICTIONARY
+declare -A arithmetic
+arithmetic[1]="$Operation1"
+arithmetic[2]="$Operation2"
+arithmetic[3]="$Operation3"
+arithmetic[4]="$Operation4"
+
+
+
