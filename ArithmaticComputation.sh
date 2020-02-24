81d19911e9c2e28aeeee4775d805acb641b4b0a6
vkshay3400 <akshayp2208@gmail.com>
2020-02-24 15:16:03 +0530

    Modified: Changed name of operations

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -3,34 +3,29 @@
 echo "*************************ArithmaticComputation*************************"
 
 # THREE INPUTS FOR PERFORMING OPERATIONS
-read -p "Enter first number input: " firstnumber
-read -p "Enter second number input: " secondnumber
-read -p "Enter third number input: " thirdnumber
+read -p "Enter first number input: " input1
+read -p "Enter second number input: " input2
+read -p "Enter third number input: " input3
 
 # ARITHMETIC OPERATION
-Operation1=`echo "scale=2; $firstnumber + $secondnumber * $thirdnumber" | bc`
-Operation2=`echo "scale=2; $firstnumber * $secondnumber + $thirdnumber" | bc`
-Operation3=`echo "scale=2; $thirdnumber + $firstnumber / $secondnumber" | bc`
-Operation4=`echo "scale=2; $firstnumber % $secondnumber + $thirdnumber" | bc`
-
-# DISPLAY OPERATION
-echo "Operations Output: " $Operation1 $Operation2 $Operation3 $Operation4
+operation1=`echo "scale=2; $input1 + $input2 * $input3" | bc`
+operation2=`echo "scale=2; $input1 * $input2 + $input3" | bc`
+operation3=`echo "scale=2; $input3 + $input1 / $input2" | bc`
+operation4=`echo "scale=2; $input1 % $input2 + $input3" | bc`
 
 # STORE IN DICTIONARY
 declare -A arithmetic
-arithmetic[1]="$Operation1"
-arithmetic[2]="$Operation2"
-arithmetic[3]="$Operation3"
-arithmetic[4]="$Operation4"
+arithmetic[1]="$operation1"
+arithmetic[2]="$operation2"
+arithmetic[3]="$operation3"
+arithmetic[4]="$operation4"
 
 # DISPLAY DICTIONARY INTO ARRAY
 length=${#arithmetic[@]}
-echo $length
 for (( index=0; index<$length; index++ ))
 do
 	resultArray[index]=${arithmetic[$((index+1))]}
 done
-echo ${resultArray[@]}
 
 # SORT IN DESCENDING ORDER
 for (( index1=0; index1<$length; index1++ ))
@@ -61,4 +56,3 @@ do
 	done
 done
 echo "Ascending values are " ${resultArray[@]}
-
